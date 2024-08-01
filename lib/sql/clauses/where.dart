b8b1578702074672b87b09abae6d6c380cb3c892
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-08-01 20:37:05 +0200

    Changed lowerCase implementation of Where class

--- lib/sql/clauses/where.dart
+++ lib/sql/clauses/where.dart
@@ -1,6 +1,6 @@
 class WhereCollection {
   chain() {
-    return "WHERE ${wheres.map((where) => "${where.field} ${where.comporator.operator()} ${where.lowerCase ? where.getCompareTo.toLowerCase() : where.getCompareTo}").join(" AND ")}";
+    return "WHERE ${wheres.map((where) => where.lowerCase ? "${where.field} ILIKE ${where.getCompareTo}" : "${where.field} ${where.comporator.operator()} ${where.getCompareTo}").join(" AND ")}";
   }
 
   List<Where> wheres;
