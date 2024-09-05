91cb87e3fdd9f99d48a787420189546db3fdd841
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-09-05 17:25:51 +0200

    Added CaseSensitive logic for where

--- lib/where/statement.dart
+++ lib/where/statement.dart
@@ -1,3 +1,4 @@
+import 'package:dart_store/dart_store.dart';
 import 'package:dart_store/data_definition/table/column/internal.dart';
 import 'package:dart_store/statement/statement.dart';
 import 'package:dart_store/where/comparison_operator.dart';
@@ -7,13 +8,14 @@ class Where<ForeignField extends Type?> extends Statement {
   Where(
       {required this.comparisonOperator,
       required this.internalColumn,
-      required this.value})
+      required this.value,
+      this.caseSensitive = true})
       : foreignField = ForeignField;
   final Type? foreignField;
   final ComparisonOperator comparisonOperator;
   final InternalColumn internalColumn;
   final dynamic value;
-
+  final bool caseSensitive;
   @override
   String define() {
     if (!internalColumn.dataType.compareToValue(value)) {
@@ -21,7 +23,9 @@ class Where<ForeignField extends Type?> extends Statement {
           "Value $value is not compoarible to column ${internalColumn.sqlName} of type ${internalColumn.dataType}");
     }
     final convertedValue = internalColumn.dataType.convert(value);
-
+    if (!caseSensitive && (internalColumn.dataType is Varchar)) {
+      return "LOWER(${internalColumn.sqlName}) ${comparisonOperator.operator()} LOWER('$convertedValue')";
+    }
     return "${internalColumn.sqlName} ${comparisonOperator.operator()} $convertedValue";
   }
 }
