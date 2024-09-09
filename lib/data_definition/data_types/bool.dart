c969ac5ed3520666fdff93ec500d7cd0dab27c03
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-09-09 13:05:06 +0200

    Added debug code

--- lib/data_definition/data_types/bool.dart
+++ lib/data_definition/data_types/bool.dart
@@ -1,10 +1,15 @@
 import 'package:dart_store/data_definition/data_types/data_type.dart';
 
-class Bool extends SQLDataType {
+class Bool extends SQLDataType<bool> {
   const Bool({super.isNullable = false});
 
   @override
-  convert(value) {
-    return value == 0 ? 'false' : 'true';
+  convert(bool? value) {
+    if (value == null && isNullable == true) {
+      return null;
+    } else if (value == null && isNullable == false) {
+      throw Exception("Value cannot be null");
+    }
+    return value.toString() == "true" ? 'true' : 'false';
   }
 }
