8cbc119dbe6c953b4569551f5f6f69ded85ad4d4
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-09-12 15:25:55 +0200

    fixed querying from foreign fields

--- lib/data_query/many_to_one/service.dart
+++ lib/data_query/many_to_one/service.dart
@@ -55,7 +55,7 @@ class ManyToOneQueryService with DartStoreUtility {
 
       final connectionName = oneToManyDefinition.connectionName;
       final statement =
-          "SELECT ${referencedTableDescription.tableName}.id as id, ${referencedTableDescription.internalColumnsSqlNamesWithoutId} FROM ${referencedTableDescription.tableName} JOIN $connectionName ON $connectionName.${referencedTableDescription.tableName} =${referencedTableDescription.tableName}.id WHERE $connectionName.${referencedTableDescription.tableName} = ${pKey.dataType.convert(pKeyValue)} ${WhereService().defineAndChainWhereStatements(where: filteredWhere).replaceAll("WHERE", "AND")}";
+          "SELECT ${referencedTableDescription.tableName}.id as id, ${referencedTableDescription.internalColumnsSqlNamesWithoutId} FROM ${referencedTableDescription.tableName} JOIN $connectionName ON $connectionName.${referencedTableDescription.tableName} =${referencedTableDescription.tableName}.id WHERE $connectionName.${entityInstance.tableName} = ${pKey.dataType.convert(pKeyValue)} ${WhereService().defineAndChainWhereStatements(where: filteredWhere).replaceAll("WHERE", "AND")}";
       print(statement);
       final Result result = await executeSQL(statement);
 
