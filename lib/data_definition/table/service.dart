42b94afd94d5d7048931946fb76ae9e1680acd2e
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-09-18 20:05:47 +0200

    extended debug print for table search

--- lib/data_definition/table/service.dart
+++ lib/data_definition/table/service.dart
@@ -1,16 +1,48 @@
-import 'package:dart_conversion/method_service.dart';
+import 'dart:mirrors';
+
 import 'package:dart_store/data_definition/constraint/service.dart';
 import 'package:dart_store/data_definition/table/column/column.dart';
-import 'package:dart_store/data_definition/table/column/service.dart';
 import 'package:dart_store/data_definition/table/column/internal.dart';
+import 'package:dart_store/data_definition/table/column/service.dart';
 import 'package:dart_store/data_definition/table/entity.dart';
 import 'package:dart_store/data_definition/table/table_description.dart';
 import 'package:dart_store/data_definition/table/table_statement.dart';
 import 'package:dart_store/reflection/collector_service.dart';
-import 'package:change_case/change_case.dart';
 import 'package:dart_store/utility/dart_store_utility.dart';
 
 class TableService with DartStoreUtility {
+  Future<void> createTable(TableDescription tableDescription) async {
+    final TableStatement tableStatement = TableStatement(
+        tableDescription.tableName,
+        tableDescription.columns.whereType<InternalColumn>().toList());
+    final sql = tableStatement.define();
+    await executeSQL(sql);
+
+    await ConstraintService().postTableDefinitionAndExecution(tableDescription);
+  }
+
+  TableDescription findTable(Type tableType) {
+    final classMirror = CollectorService()
+        .searchClassesWithAnnotation<Entity>()
+        .where((classMirror) => classMirror.reflectedType == tableType)
+        .firstOrNull;
+    if (classMirror == null) {
+      throw Exception(
+          "Table not found for type $tableType with simpleName = ${MirrorSystem.getName(classMirror?.simpleName ?? Symbol("not name found"))}. Anotated with @Entity to ensure table definition");
+    }
+
+    final List<Column> columns = ColumnService().extractColumns(classMirror);
+
+    return TableDescription(
+        objectType: tableType,
+        entity: classMirror.metadata
+            .firstWhere(
+              (element) => element.reflectee is Entity,
+            )
+            .reflectee as Entity,
+        columns: columns);
+  }
+
   List<TableDescription> findTables() =>
       CollectorService().searchClassesWithAnnotation<Entity>().map(
         (classMirror) {
@@ -27,36 +59,4 @@ class TableService with DartStoreUtility {
               columns: columns);
         },
       ).toList();
-
-  TableDescription findTable(Type tableType) {
-    final classMirror = CollectorService()
-        .searchClassesWithAnnotation<Entity>()
-        .where((classMirror) => classMirror.reflectedType == tableType)
-        .firstOrNull;
-    if (classMirror == null) {
-      throw Exception(
-          "Table not found for type $tableType. Anotated with @Entity to ensure table definition");
-    }
-
-    final List<Column> columns = ColumnService().extractColumns(classMirror);
-
-    return TableDescription(
-        objectType: tableType,
-        entity: classMirror.metadata
-            .firstWhere(
-              (element) => element.reflectee is Entity,
-            )
-            .reflectee as Entity,
-        columns: columns);
-  }
-
-  Future<void> createTable(TableDescription tableDescription) async {
-    final TableStatement tableStatement = TableStatement(
-        tableDescription.tableName,
-        tableDescription.columns.whereType<InternalColumn>().toList());
-    final sql = tableStatement.define();
-    await executeSQL(sql);
-
-    await ConstraintService().postTableDefinitionAndExecution(tableDescription);
-  }
 }
