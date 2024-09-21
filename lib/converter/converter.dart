874ec24fc4877cd020289d62e264a084aa9a429f
Bilal Demirci <Bilal.demirci001@gmail.com>
2024-09-21 18:54:39 +0200

    EntityInstance is now implementing TableDescriptino

--- lib/converter/converter.dart
+++ lib/converter/converter.dart
@@ -3,105 +3,10 @@ import 'package:dart_store/connection/description/description.dart';
 import 'package:dart_store/connection/instance/instance.dart';
 import 'package:dart_store/data_definition/table/column/internal.dart';
 import 'package:dart_store/data_definition/table/table_description.dart';
-
+import 'package:dart_store/data_manipulation/entity_instance/column_instance/column_instance.dart';
 import 'package:dart_store/data_manipulation/entity_instance/column_instance/foreign/foreign.dart';
 import 'package:dart_store/data_manipulation/entity_instance/column_instance/internal_column.dart';
 import 'package:dart_store/data_manipulation/entity_instance/entity_instance.dart';
-
-extension EntityMapConverter on ConversionService {
-  EntityInstance mapToEntityInstance(
-      {required TableDescription description,
-      required Map<String, dynamic> map}) {
-    return EntityInstance(
-        objectType: description.objectType,
-        entity: description.entity,
-        columns: description.columns
-            .whereType<InternalColumn>()
-            .map(
-              (e) => InternalColumnInstance(
-                  constraints: e.constraints,
-                  name: e.name,
-                  value: map[e.name],
-                  dataType: e.dataType),
-            )
-            .toList());
-  }
-
-  List<EntityInstance> mapListToEntityInstances(
-      {required TableDescription description,
-      required List<Map<String, dynamic>> maps}) {
-    return maps
-        .map((map) => mapToEntityInstance(description: description, map: map))
-        .toList();
-  }
-
-  List<TableConnectionInstance> mapListToTableConnectionInstance(
-          {required List<Map<String, dynamic>> maps,
-          required TableConnectionDescription tableConnectionDescription}) =>
-      maps
-          .map((map) => TableConnectionInstance(
-              entity: tableConnectionDescription.entity,
-              columns: tableConnectionDescription.columns
-                  .whereType<InternalColumn>()
-                  .map(
-                    (e) => InternalColumnInstance(
-                        value: map[e.sqlName],
-                        dataType: e.dataType,
-                        constraints: e.constraints,
-                        name: e.name),
-                  )
-                  .toList()))
-          .toList();
-
-  TableConnectionInstance mapToTableConnectionInstance(
-          {required Map<String, dynamic> map,
-          required TableConnectionDescription tableConnectionDescription}) =>
-      TableConnectionInstance(
-          entity: tableConnectionDescription.entity,
-          columns: tableConnectionDescription.columns
-              .whereType<InternalColumnInstance>()
-              .map(
-                (e) => InternalColumnInstance(
-                    value: map[e.name],
-                    dataType: e.dataType,
-                    constraints: e.constraints,
-                    name: e.name),
-              )
-              .toList());
-  entityInstanceToMap(EntityInstance entityInstance) {
-    final internalColumnsInstances =
-        entityInstance.columns.whereType<InternalColumnInstance>();
-    final internalColumnsInstancesMapEntries = internalColumnsInstances
-        .map((e) => MapEntry<String, dynamic>(e.name, e.value));
-
-    final foreignColumnsInstances =
-        entityInstance.columns.whereType<ForeignColumnInstance>();
-    final foreignColumnsInstancesMapEntries = foreignColumnsInstances.map(
-        (e) => MapEntry<String, dynamic>(e.name, entityInstanceToMap(e.value)));
-
-    final instanceMap = {
-      ...internalColumnsInstancesMapEntries,
-      ...foreignColumnsInstancesMapEntries
-    };
-    return instanceMap;
-  }
-
-  entityInstanceToModel(EntityInstance entityInstance) =>
-      ConversionService.mapToObject(entityInstanceToMap(entityInstance));
-}
-
-TableConnectionInstance mapToTableConnectionInstance(
-        {required Map<String, dynamic> map,
-        required TableConnectionDescription tableConnectionDescription}) =>
-    ConversionService().mapToTableConnectionInstance(
-        map: map, tableConnectionDescription: tableConnectionDescription);
-
-List<TableConnectionInstance> mapListToTableConnectionInstance(
-    {required List<Map<String, dynamic>> maps,
-    required TableConnectionDescription tableConnectionDescription}) {
-  return ConversionService().mapListToTableConnectionInstance(
-      maps: maps, tableConnectionDescription: tableConnectionDescription);
-}
 
 Map<String, dynamic> entityInstanceToMap(EntityInstance entityInstance) {
   final internalColumnsInstances =
@@ -142,3 +47,99 @@ Map<String, dynamic> entityInstanceToMap(EntityInstance entityInstance) {
 T entityInstanceToModel<T>(EntityInstance entityInstance, {Type? type}) =>
     ConversionService.mapToObject<T>(entityInstanceToMap(entityInstance),
         type: type);
+
+List<TableConnectionInstance> mapListToTableConnectionInstance(
+    {required List<Map<String, dynamic>> maps,
+    required TableConnectionDescription tableConnectionDescription}) {
+  return ConversionService().mapListToTableConnectionInstance(
+      maps: maps, tableConnectionDescription: tableConnectionDescription);
+}
+
+TableConnectionInstance mapToTableConnectionInstance(
+        {required Map<String, dynamic> map,
+        required TableConnectionDescription tableConnectionDescription}) =>
+    ConversionService().mapToTableConnectionInstance(
+        map: map, tableConnectionDescription: tableConnectionDescription);
+
+extension EntityMapConverter on ConversionService {
+  entityInstanceToMap(EntityInstance entityInstance) {
+    final internalColumnsInstances =
+        entityInstance.columns.whereType<InternalColumnInstance>();
+    final internalColumnsInstancesMapEntries = internalColumnsInstances
+        .map((e) => MapEntry<String, dynamic>(e.name, e.value));
+
+    final foreignColumnsInstances =
+        entityInstance.columns.whereType<ForeignColumnInstance>();
+    final foreignColumnsInstancesMapEntries = foreignColumnsInstances.map(
+        (e) => MapEntry<String, dynamic>(e.name, entityInstanceToMap(e.value)));
+
+    final instanceMap = {
+      ...internalColumnsInstancesMapEntries,
+      ...foreignColumnsInstancesMapEntries
+    };
+    return instanceMap;
+  }
+
+  entityInstanceToModel(EntityInstance entityInstance) =>
+      ConversionService.mapToObject(entityInstanceToMap(entityInstance));
+
+  List<EntityInstance> mapListToEntityInstances(
+      {required TableDescription description,
+      required List<Map<String, dynamic>> maps}) {
+    return maps
+        .map((map) => mapToEntityInstance(description: description, map: map))
+        .toList();
+  }
+
+  List<TableConnectionInstance> mapListToTableConnectionInstance(
+          {required List<Map<String, dynamic>> maps,
+          required TableConnectionDescription tableConnectionDescription}) =>
+      maps
+          .map((map) => TableConnectionInstance(
+              entity: tableConnectionDescription.entity,
+              columns: tableConnectionDescription.columns
+                  .whereType<InternalColumn>()
+                  .map(
+                    (e) => InternalColumnInstance(
+                        value: map[e.sqlName],
+                        dataType: e.dataType,
+                        constraints: e.constraints,
+                        name: e.name),
+                  )
+                  .toList()))
+          .toList();
+  EntityInstance mapToEntityInstance(
+      {required TableDescription description,
+      required Map<String, dynamic> map}) {
+    return EntityInstance(
+        objectType: description.objectType,
+        entity: description.entity,
+        columns: List.castFrom<InternalColumnInstance, ColumnInstance>(
+            description.columns
+                .whereType<InternalColumn>()
+                .map(
+                  (e) => InternalColumnInstance(
+                      constraints: e.constraints,
+                      name: e.name,
+                      value: map[e.name],
+                      dataType: e.dataType),
+                )
+                .toList()));
+  }
+
+  TableConnectionInstance mapToTableConnectionInstance(
+          {required Map<String, dynamic> map,
+          required TableConnectionDescription tableConnectionDescription}) =>
+      TableConnectionInstance(
+          entity: tableConnectionDescription.entity,
+          columns: tableConnectionDescription.columns
+              .whereType<InternalColumnInstance>()
+              .map(
+                (e) => InternalColumnInstance(
+                    value: map[e.name],
+                    dataType: e.dataType,
+                    constraints: e.constraints,
+                    name: e.name),
+              )
+              .toList());
+}
